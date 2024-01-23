d663ca33d36ecafaa34fff33ee9933df4c69b449
Ramavhale Murendeni <Ramavhale Murendeni@DESKTOP-CO1SVRB>
2024-01-23 08:55:55 +0200

    Add project files.

---
+++ Form1.Designer.vb
@@ -0,0 +1,470 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        txtDisplay = New TextBox()
+        btnAdd = New Button()
+        btnSubtract = New Button()
+        btnMultiply = New Button()
+        btnDivide = New Button()
+        btnEquals = New Button()
+        btnClear = New Button()
+        btn1 = New Button()
+        btn2 = New Button()
+        btn3 = New Button()
+        btn4 = New Button()
+        btn5 = New Button()
+        btn6 = New Button()
+        btn7 = New Button()
+        btn8 = New Button()
+        btn9 = New Button()
+        btn0 = New Button()
+        btnDecimal = New Button()
+        btnSqrt = New Button()
+        btnBackspace = New Button()
+        btnPercentage = New Button()
+        btnNegate = New Button()
+        btnMemorySave = New Button()
+        btnMemoryRecall = New Button()
+        btnMemoryClear = New Button()
+        btnSin = New Button()
+        btnCos = New Button()
+        btnTan = New Button()
+        btnExp = New Button()
+        btnLog = New Button()
+        btnPi = New Button()
+        btnE = New Button()
+        btnOpenParen = New Button()
+        btnCloseParen = New Button()
+        Button1 = New Button()
+        Button2 = New Button()
+        SuspendLayout()
+        ' 
+        ' txtDisplay
+        ' 
+        txtDisplay.Location = New Point(25, 66)
+        txtDisplay.Name = "txtDisplay"
+        txtDisplay.Size = New Size(319, 23)
+        txtDisplay.TabIndex = 0
+        ' 
+        ' btnAdd
+        ' 
+        btnAdd.Location = New Point(370, 120)
+        btnAdd.Name = "btnAdd"
+        btnAdd.Size = New Size(88, 49)
+        btnAdd.TabIndex = 1
+        btnAdd.Text = "+"
+        btnAdd.UseVisualStyleBackColor = True
+        ' 
+        ' btnSubtract
+        ' 
+        btnSubtract.Location = New Point(370, 184)
+        btnSubtract.Name = "btnSubtract"
+        btnSubtract.Size = New Size(88, 41)
+        btnSubtract.TabIndex = 2
+        btnSubtract.Text = "-"
+        btnSubtract.UseVisualStyleBackColor = True
+        ' 
+        ' btnMultiply
+        ' 
+        btnMultiply.Location = New Point(370, 242)
+        btnMultiply.Name = "btnMultiply"
+        btnMultiply.Size = New Size(88, 39)
+        btnMultiply.TabIndex = 3
+        btnMultiply.Text = "*"
+        btnMultiply.UseVisualStyleBackColor = True
+        ' 
+        ' btnDivide
+        ' 
+        btnDivide.Location = New Point(370, 298)
+        btnDivide.Name = "btnDivide"
+        btnDivide.Size = New Size(83, 44)
+        btnDivide.TabIndex = 4
+        btnDivide.Text = "/"
+        btnDivide.UseVisualStyleBackColor = True
+        ' 
+        ' btnEquals
+        ' 
+        btnEquals.Location = New Point(370, 365)
+        btnEquals.Name = "btnEquals"
+        btnEquals.Size = New Size(88, 36)
+        btnEquals.TabIndex = 5
+        btnEquals.Text = "="
+        btnEquals.UseVisualStyleBackColor = True
+        ' 
+        ' btnClear
+        ' 
+        btnClear.Location = New Point(370, 52)
+        btnClear.Name = "btnClear"
+        btnClear.Size = New Size(88, 51)
+        btnClear.TabIndex = 6
+        btnClear.Text = "Clear"
+        btnClear.UseVisualStyleBackColor = True
+        ' 
+        ' btn1
+        ' 
+        btn1.Location = New Point(25, 120)
+        btn1.Name = "btn1"
+        btn1.Size = New Size(99, 49)
+        btn1.TabIndex = 7
+        btn1.Text = "1"
+        btn1.UseVisualStyleBackColor = True
+        ' 
+        ' btn2
+        ' 
+        btn2.Location = New Point(154, 124)
+        btn2.Name = "btn2"
+        btn2.Size = New Size(75, 49)
+        btn2.TabIndex = 8
+        btn2.Text = "2"
+        btn2.UseVisualStyleBackColor = True
+        ' 
+        ' btn3
+        ' 
+        btn3.Location = New Point(254, 122)
+        btn3.Name = "btn3"
+        btn3.Size = New Size(90, 51)
+        btn3.TabIndex = 9
+        btn3.Text = "3"
+        btn3.UseVisualStyleBackColor = True
+        ' 
+        ' btn4
+        ' 
+        btn4.Location = New Point(25, 200)
+        btn4.Name = "btn4"
+        btn4.Size = New Size(99, 45)
+        btn4.TabIndex = 10
+        btn4.Text = "4"
+        btn4.UseVisualStyleBackColor = True
+        ' 
+        ' btn5
+        ' 
+        btn5.Location = New Point(154, 200)
+        btn5.Name = "btn5"
+        btn5.Size = New Size(75, 45)
+        btn5.TabIndex = 11
+        btn5.Text = "5"
+        btn5.UseVisualStyleBackColor = True
+        ' 
+        ' btn6
+        ' 
+        btn6.Location = New Point(254, 200)
+        btn6.Name = "btn6"
+        btn6.Size = New Size(90, 45)
+        btn6.TabIndex = 12
+        btn6.Text = "6"
+        btn6.UseVisualStyleBackColor = True
+        ' 
+        ' btn7
+        ' 
+        btn7.Location = New Point(25, 284)
+        btn7.Name = "btn7"
+        btn7.Size = New Size(99, 44)
+        btn7.TabIndex = 13
+        btn7.Text = "7"
+        btn7.UseVisualStyleBackColor = True
+        ' 
+        ' btn8
+        ' 
+        btn8.ImageAlign = ContentAlignment.MiddleRight
+        btn8.Location = New Point(154, 284)
+        btn8.Name = "btn8"
+        btn8.Size = New Size(75, 44)
+        btn8.TabIndex = 14
+        btn8.Text = "8"
+        btn8.UseVisualStyleBackColor = True
+        ' 
+        ' btn9
+        ' 
+        btn9.Location = New Point(254, 284)
+        btn9.Name = "btn9"
+        btn9.Size = New Size(90, 44)
+        btn9.TabIndex = 15
+        btn9.Text = "9"
+        btn9.UseVisualStyleBackColor = True
+        ' 
+        ' btn0
+        ' 
+        btn0.Location = New Point(154, 362)
+        btn0.Name = "btn0"
+        btn0.Size = New Size(75, 43)
+        btn0.TabIndex = 16
+        btn0.Text = "0"
+        btn0.UseVisualStyleBackColor = True
+        ' 
+        ' btnDecimal
+        ' 
+        btnDecimal.Location = New Point(464, 66)
+        btnDecimal.Name = "btnDecimal"
+        btnDecimal.Size = New Size(91, 23)
+        btnDecimal.TabIndex = 17
+        btnDecimal.Text = "Decimal"
+        btnDecimal.UseVisualStyleBackColor = True
+        ' 
+        ' btnSqrt
+        ' 
+        btnSqrt.Location = New Point(573, 66)
+        btnSqrt.Name = "btnSqrt"
+        btnSqrt.Size = New Size(86, 23)
+        btnSqrt.TabIndex = 18
+        btnSqrt.Text = "Sqrt"
+        btnSqrt.UseVisualStyleBackColor = True
+        ' 
+        ' btnBackspace
+        ' 
+        btnBackspace.Location = New Point(464, 122)
+        btnBackspace.Name = "btnBackspace"
+        btnBackspace.Size = New Size(91, 23)
+        btnBackspace.TabIndex = 20
+        btnBackspace.Text = "Backspace"
+        btnBackspace.UseVisualStyleBackColor = True
+        ' 
+        ' btnPercentage
+        ' 
+        btnPercentage.Location = New Point(576, 122)
+        btnPercentage.Name = "btnPercentage"
+        btnPercentage.Size = New Size(93, 23)
+        btnPercentage.TabIndex = 21
+        btnPercentage.Text = "Percent"
+        btnPercentage.UseVisualStyleBackColor = True
+        ' 
+        ' btnNegate
+        ' 
+        btnNegate.Location = New Point(682, 120)
+        btnNegate.Name = "btnNegate"
+        btnNegate.Size = New Size(106, 23)
+        btnNegate.TabIndex = 22
+        btnNegate.Text = "Negate"
+        btnNegate.UseVisualStyleBackColor = True
+        ' 
+        ' btnMemorySave
+        ' 
+        btnMemorySave.Location = New Point(464, 184)
+        btnMemorySave.Name = "btnMemorySave"
+        btnMemorySave.Size = New Size(91, 23)
+        btnMemorySave.TabIndex = 23
+        btnMemorySave.Text = "MemorySave"
+        btnMemorySave.UseVisualStyleBackColor = True
+        ' 
+        ' btnMemoryRecall
+        ' 
+        btnMemoryRecall.Location = New Point(576, 184)
+        btnMemoryRecall.Name = "btnMemoryRecall"
+        btnMemoryRecall.Size = New Size(93, 23)
+        btnMemoryRecall.TabIndex = 24
+        btnMemoryRecall.Text = "MemoryRecall"
+        btnMemoryRecall.UseVisualStyleBackColor = True
+        ' 
+        ' btnMemoryClear
+        ' 
+        btnMemoryClear.Location = New Point(682, 184)
+        btnMemoryClear.Name = "btnMemoryClear"
+        btnMemoryClear.Size = New Size(106, 23)
+        btnMemoryClear.TabIndex = 25
+        btnMemoryClear.Text = "MemoryClear"
+        btnMemoryClear.UseVisualStyleBackColor = True
+        ' 
+        ' btnSin
+        ' 
+        btnSin.Location = New Point(464, 242)
+        btnSin.Name = "btnSin"
+        btnSin.Size = New Size(91, 23)
+        btnSin.TabIndex = 26
+        btnSin.Text = "Sin"
+        btnSin.UseVisualStyleBackColor = True
+        ' 
+        ' btnCos
+        ' 
+        btnCos.Location = New Point(576, 242)
+        btnCos.Name = "btnCos"
+        btnCos.Size = New Size(93, 23)
+        btnCos.TabIndex = 27
+        btnCos.Text = "Cos"
+        btnCos.UseVisualStyleBackColor = True
+        ' 
+        ' btnTan
+        ' 
+        btnTan.Location = New Point(682, 242)
+        btnTan.Name = "btnTan"
+        btnTan.Size = New Size(106, 23)
+        btnTan.TabIndex = 28
+        btnTan.Text = "Tan"
+        btnTan.UseVisualStyleBackColor = True
+        ' 
+        ' btnExp
+        ' 
+        btnExp.Location = New Point(464, 295)
+        btnExp.Name = "btnExp"
+        btnExp.Size = New Size(91, 23)
+        btnExp.TabIndex = 29
+        btnExp.Text = "Exp"
+        btnExp.UseVisualStyleBackColor = True
+        ' 
+        ' btnLog
+        ' 
+        btnLog.Location = New Point(576, 298)
+        btnLog.Name = "btnLog"
+        btnLog.Size = New Size(93, 23)
+        btnLog.TabIndex = 30
+        btnLog.Text = "Log"
+        btnLog.UseVisualStyleBackColor = True
+        ' 
+        ' btnPi
+        ' 
+        btnPi.Location = New Point(682, 66)
+        btnPi.Name = "btnPi"
+        btnPi.Size = New Size(106, 23)
+        btnPi.TabIndex = 31
+        btnPi.Text = "Pi"
+        btnPi.UseVisualStyleBackColor = True
+        ' 
+        ' btnE
+        ' 
+        btnE.Location = New Point(682, 295)
+        btnE.Name = "btnE"
+        btnE.Size = New Size(106, 23)
+        btnE.TabIndex = 32
+        btnE.Text = "E"
+        btnE.UseVisualStyleBackColor = True
+        ' 
+        ' btnOpenParen
+        ' 
+        btnOpenParen.Location = New Point(487, 347)
+        btnOpenParen.Name = "btnOpenParen"
+        btnOpenParen.Size = New Size(117, 23)
+        btnOpenParen.TabIndex = 33
+        btnOpenParen.Text = "("
+        btnOpenParen.UseVisualStyleBackColor = True
+        ' 
+        ' btnCloseParen
+        ' 
+        btnCloseParen.Location = New Point(624, 347)
+        btnCloseParen.Name = "btnCloseParen"
+        btnCloseParen.Size = New Size(107, 23)
+        btnCloseParen.TabIndex = 34
+        btnCloseParen.Text = ")"
+        btnCloseParen.UseVisualStyleBackColor = True
+        ' 
+        ' Button1
+        ' 
+        Button1.Location = New Point(254, 362)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(90, 43)
+        Button1.TabIndex = 35
+        Button1.Text = "Power OFF"
+        Button1.UseVisualStyleBackColor = True
+        ' 
+        ' Button2
+        ' 
+        Button2.Location = New Point(25, 360)
+        Button2.Name = "Button2"
+        Button2.Size = New Size(99, 45)
+        Button2.TabIndex = 36
+        Button2.Text = "ON"
+        Button2.UseVisualStyleBackColor = True
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        BackColor = Color.RoyalBlue
+        ClientSize = New Size(800, 450)
+        Controls.Add(Button2)
+        Controls.Add(Button1)
+        Controls.Add(btnCloseParen)
+        Controls.Add(btnOpenParen)
+        Controls.Add(btnE)
+        Controls.Add(btnPi)
+        Controls.Add(btnLog)
+        Controls.Add(btnExp)
+        Controls.Add(btnTan)
+        Controls.Add(btnCos)
+        Controls.Add(btnSin)
+        Controls.Add(btnMemoryClear)
+        Controls.Add(btnMemoryRecall)
+        Controls.Add(btnMemorySave)
+        Controls.Add(btnNegate)
+        Controls.Add(btnPercentage)
+        Controls.Add(btnBackspace)
+        Controls.Add(btnSqrt)
+        Controls.Add(btnDecimal)
+        Controls.Add(btn0)
+        Controls.Add(btn9)
+        Controls.Add(btn8)
+        Controls.Add(btn7)
+        Controls.Add(btn6)
+        Controls.Add(btn5)
+        Controls.Add(btn4)
+        Controls.Add(btn3)
+        Controls.Add(btn2)
+        Controls.Add(btn1)
+        Controls.Add(btnClear)
+        Controls.Add(btnEquals)
+        Controls.Add(btnDivide)
+        Controls.Add(btnMultiply)
+        Controls.Add(btnSubtract)
+        Controls.Add(btnAdd)
+        Controls.Add(txtDisplay)
+        Name = "Form1"
+        Text = "CALCULATER"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents txtDisplay As TextBox
+    Friend WithEvents btnAdd As Button
+    Friend WithEvents btnSubtract As Button
+    Friend WithEvents btnMultiply As Button
+    Friend WithEvents btnDivide As Button
+    Friend WithEvents btnEquals As Button
+    Friend WithEvents btnClear As Button
+    Friend WithEvents btn1 As Button
+    Friend WithEvents btn2 As Button
+    Friend WithEvents btn3 As Button
+    Friend WithEvents btn4 As Button
+    Friend WithEvents btn5 As Button
+    Friend WithEvents btn6 As Button
+    Friend WithEvents btn7 As Button
+    Friend WithEvents btn8 As Button
+    Friend WithEvents btn9 As Button
+    Friend WithEvents btn0 As Button
+    Friend WithEvents btnDecimal As Button
+    Friend WithEvents btnSqrt As Button
+    Friend WithEvents btnBackspace As Button
+    Friend WithEvents btnPercentage As Button
+    Friend WithEvents btnNegate As Button
+    Friend WithEvents btnMemorySave As Button
+    Friend WithEvents btnMemoryRecall As Button
+    Friend WithEvents btnMemoryClear As Button
+    Friend WithEvents btnSin As Button
+    Friend WithEvents btnCos As Button
+    Friend WithEvents btnTan As Button
+    Friend WithEvents btnExp As Button
+    Friend WithEvents btnLog As Button
+    Friend WithEvents btnPi As Button
+    Friend WithEvents btnE As Button
+    Friend WithEvents btnOpenParen As Button
+    Friend WithEvents btnCloseParen As Button
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+End Class
---
+++ Form1.vb
@@ -0,0 +1,193 @@
+﻿Public Class Form1
+
+    ' Variable to store the first operand
+    Private operand1 As Double
+    ' Variable to store the selected operation (+, -, *, /)
+    Private operation As String
+    Private memory As Double = 0
+
+    Private Sub btnNumber_Click(sender As Object, e As EventArgs) Handles btn0.Click, btn1.Click, btn2.Click, btn3.Click, btn4.Click, btn5.Click, btn6.Click, btn7.Click, btn8.Click, btn9.Click
+        ' Handle digit buttons
+        Dim button As Button = DirectCast(sender, Button)
+        txtDisplay.Text &= button.Text
+    End Sub
+
+    Private Sub btnDecimal_Click(sender As Object, e As EventArgs) Handles btnDecimal.Click
+        If Not txtDisplay.Text.Contains(".") Then
+            txtDisplay.Text &= "."
+        End If
+    End Sub
+
+    Private Sub btnBackspace_Click(sender As Object, e As EventArgs) Handles btnBackspace.Click
+        If txtDisplay.Text.Length > 0 Then
+            txtDisplay.Text = txtDisplay.Text.Remove(txtDisplay.Text.Length - 1, 1)
+        End If
+    End Sub
+
+
+
+    Private Sub btnOperation_Click(sender As Object, e As EventArgs) Handles btnAdd.Click, btnSubtract.Click, btnMultiply.Click, btnDivide.Click
+        ' Handle operation buttons
+        Dim button As Button = DirectCast(sender, Button)
+        operand1 = CDbl(txtDisplay.Text)
+        operation = button.Text
+        txtDisplay.Clear()
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub btnClear_Click(sender As Object, e As EventArgs) Handles btnClear.Click
+
+        ' Clear the display and reset variables
+        txtDisplay.Clear()
+        operand1 = 0
+        operation = ""
+
+    End Sub
+
+    Private Sub btnEquals_Click(sender As Object, e As EventArgs) Handles btnEquals.Click
+        ' Perform the calculation based on the selected operation
+        If Not String.IsNullOrEmpty(operation) AndAlso Double.TryParse(txtDisplay.Text, Nothing) Then
+            Dim operand2 As Double = CDbl(txtDisplay.Text)
+            Dim result As Double
+
+            Select Case operation
+                Case "+"
+                    result = operand1 + operand2
+                Case "-"
+                    result = operand1 - operand2
+                Case "*"
+                    result = operand1 * operand2
+                Case "/"
+                    If operand2 <> 0 Then
+                        result = operand1 / operand2
+                    Else
+                        MessageBox.Show("Cannot divide by zero.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+                        Exit Sub
+                    End If
+            End Select
+
+            ' Display the result
+            txtDisplay.Text = result.ToString()
+            ' Reset variables
+            operand1 = 0
+            operation = ""
+        End If
+
+    End Sub
+
+
+    Private Sub btnSqrt_Click(sender As Object, e As EventArgs) Handles btnSqrt.Click
+        If Double.TryParse(txtDisplay.Text, Nothing) AndAlso CDbl(txtDisplay.Text) >= 0 Then
+            txtDisplay.Text = Math.Sqrt(CDbl(txtDisplay.Text)).ToString()
+        Else
+            MessageBox.Show("Invalid input for square root.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+        End If
+    End Sub
+
+    Private Sub btnPercentage_Click(sender As Object, e As EventArgs) Handles btnPercentage.Click
+        If Double.TryParse(txtDisplay.Text, Nothing) Then
+            txtDisplay.Text = (CDbl(txtDisplay.Text) / 100).ToString()
+        Else
+            MessageBox.Show("Invalid input for percentage.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+        End If
+    End Sub
+
+    Private Sub btnNegate_Click(sender As Object, e As EventArgs) Handles btnNegate.Click
+        If Double.TryParse(txtDisplay.Text, Nothing) Then
+            txtDisplay.Text = (-CDbl(txtDisplay.Text)).ToString()
+        Else
+            MessageBox.Show("Invalid input for negation.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+        End If
+    End Sub
+
+    Private Sub btnMemorySave_Click(sender As Object, e As EventArgs) Handles btnMemorySave.Click
+        If Double.TryParse(txtDisplay.Text, Nothing) Then
+            memory = CDbl(txtDisplay.Text)
+        Else
+            MessageBox.Show("Invalid input for memory save.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+        End If
+    End Sub
+
+    Private Sub btnMemoryRecall_Click(sender As Object, e As EventArgs) Handles btnMemoryRecall.Click
+        txtDisplay.Text = memory.ToString()
+    End Sub
+
+    Private Sub btnMemoryClear_Click(sender As Object, e As EventArgs) Handles btnMemoryClear.Click
+        memory = 0
+    End Sub
+
+    Private Sub btnSin_Click(sender As Object, e As EventArgs) Handles btnSin.Click
+        If Double.TryParse(txtDisplay.Text, Nothing) Then
+            txtDisplay.Text = Math.Sin(CDbl(txtDisplay.Text)).ToString()
+        Else
+            MessageBox.Show("Invalid input for sine.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+        End If
+    End Sub
+
+    Private Sub btnCos_Click(sender As Object, e As EventArgs) Handles btnCos.Click
+        If Double.TryParse(txtDisplay.Text, Nothing) Then
+            txtDisplay.Text = Math.Cos(CDbl(txtDisplay.Text)).ToString()
+        Else
+            MessageBox.Show("Invalid input for cosine.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+        End If
+    End Sub
+
+    Private Sub btnTan_Click(sender As Object, e As EventArgs) Handles btnTan.Click
+        If Double.TryParse(txtDisplay.Text, Nothing) Then
+            txtDisplay.Text = Math.Tan(CDbl(txtDisplay.Text)).ToString()
+        Else
+            MessageBox.Show("Invalid input for tangent.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+        End If
+    End Sub
+
+    Private Sub btnExp_Click(sender As Object, e As EventArgs) Handles btnExp.Click
+        If Double.TryParse(txtDisplay.Text, Nothing) Then
+            txtDisplay.Text = Math.Exp(CDbl(txtDisplay.Text)).ToString()
+        Else
+            MessageBox.Show("Invalid input for exponential.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+        End If
+    End Sub
+
+    Private Sub btnLog_Click(sender As Object, e As EventArgs) Handles btnLog.Click
+        If Double.TryParse(txtDisplay.Text, Nothing) AndAlso CDbl(txtDisplay.Text) > 0 Then
+            txtDisplay.Text = Math.Log(CDbl(txtDisplay.Text)).ToString()
+        Else
+            MessageBox.Show("Invalid input for logarithm.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+        End If
+    End Sub
+
+    Private Sub btnPi_Click(sender As Object, e As EventArgs) Handles btnPi.Click
+        txtDisplay.Text = Math.PI.ToString()
+    End Sub
+
+    Private Sub btnE_Click(sender As Object, e As EventArgs) Handles btnE.Click
+        txtDisplay.Text = Math.E.ToString()
+    End Sub
+
+    Private Sub btnOpenParen_Click(sender As Object, e As EventArgs) Handles btnOpenParen.Click
+        txtDisplay.Text &= "("
+    End Sub
+
+    Private Sub btnCloseParen_Click(sender As Object, e As EventArgs) Handles btnCloseParen.Click
+        txtDisplay.Text &= ")"
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+
+        Close()
+
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+
+        ' Clear the display and reset variables
+        txtDisplay.Clear()
+        operand1 = 0
+        operation = ""
+        operation = ""
+
+    End Sub
+End Class
